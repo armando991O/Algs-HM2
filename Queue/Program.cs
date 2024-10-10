8887c09d952f7665d0636981389291261dcef9ab
Arman <117938877+armando991O@users.noreply.github.com>
2024-10-10 20:30:34 +0400

    Add files via upload

---
+++ Queue/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+class Program
+{
+    
+}
+
+public class Queue<T>
+{
+    private List<T> elements;
+
+    public Queue()
+    {
+        elements = new List<T>();
+    }
+
+    public bool isEmpty()
+    {
+        return elements.Count == 0;
+    }
+
+    public void Enqueue(T item)
+    {
+        elements.Add(item);
+    }
+
+    public T Dequeue()
+    {
+        if (isEmpty())
+        {
+            Console.WriteLine("Queue is empty! You can't dequeue");
+        }
+
+        T frontItem = elements[0];
+        elements.RemoveAt(0);
+
+        return frontItem;
+    }
+
+    public T Peek()
+    {
+        if (isEmpty())
+        {
+            Console.WriteLine("Queue is empty");
+        }
+
+        T frontElement = elements[0];
+
+        return frontElement;
+    }
+
+    public int Size()
+    {
+        return elements.Count;
+    }
+}
---
+++ sorting/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class Program
+{
+    static void insertionSortRecursive(int[] arr, int n)
+    {
+        if (n <= 1)
+            return;
+
+        insertionSortRecursive(arr, n - 1);
+
+        int last = arr[n - 1];
+        int j = n - 2;
+
+        while (j >= 0 && arr[j] > last)
+        {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+
+        arr[j + 1] = last;
+    }
+
+    static void Main()
+    {
+        int[] arr = { 8, 4, 1, 18, 17, 19, 3 };
+
+        insertionSortRecursive(arr, arr.Length);
+
+        foreach (int n in arr)
+        {
+            Console.WriteLine(n);
+        }
+    }
+}
